162a2ca6e49f433157b2af5f11ca59de201e3370
nittin4664 <nittin4664@gmail.com>
2012-07-31 18:57:36 +0530

    test case for bookshelf

--- test/com/twu28/biblioteca/BookShelfTest.java
+++ test/com/twu28/biblioteca/BookShelfTest.java
@@ -10,7 +10,10 @@ import junit.framework.TestCase;
  * To change this template use File | Settings | File Templates.
  */
 public class BookShelfTest extends TestCase {
-
+    public void testAsStringFor0book() throws Exception {
+        BookShelf bookShelf=new BookShelf();
+        assertEquals("", bookShelf.bookAsString());
+    }
     public void testAsStringFor1book() throws Exception {
         BookShelf bookShelf=new BookShelf();
         bookShelf.addBook(new Book("sins of the father"));
@@ -22,10 +25,20 @@ public class BookShelfTest extends TestCase {
         bookShelf.addBook(new Book("the jackal"));
         assertEquals("sins of the father|the jackal|", bookShelf.bookAsString());
     }
+    public void testSearchBookWhenNoBook(){
+        BookShelf bookShelf=new BookShelf();
+        assertEquals(null,bookShelf.searchBook("sins of the father"));
+    }
     public void testSearchBookFormSingleBook(){
         BookShelf bookShelf=new BookShelf();
         bookShelf.addBook(new Book("sins of the father"));
         assertEquals("sins of the father",bookShelf.searchBook("sins of the father"));
+    }
+    public void testSearchBookFormShelfWhenBookNotPresent(){
+        BookShelf bookShelf=new BookShelf();
+        bookShelf.addBook(new Book("sins of the father"));
+        bookShelf.addBook(new Book("only time will tell"));
+        assertEquals(null,bookShelf.searchBook("the jackal"));
     }
     public void testSearchBookFormMoreThenOneBook(){
         BookShelf bookShelf=new BookShelf();
