aae1372c2a79f9e59b6b1936d9d2e66be792f178
nittin4664 <nittin4664@gmail.com>
2012-07-31 18:28:43 +0530

    test case for lendbook when not present on the shelf

--- test/com/twu28/biblioteca/LibraryTest.java
+++ test/com/twu28/biblioteca/LibraryTest.java
@@ -42,12 +42,14 @@ public class LibraryTest extends TestCase {
      Library library=new Library();
        assertEquals("book not found",library.lendBook("only time will tell"));
     }
+
     @Test
-    public void testLendBookWhenShelfNotEmpty(){
+    public void testLendBookWhenShelfNotEmptyButBookNotPresent(){
         Library library=new Library();
         library.addBookToShelf(new Book("only time will tell"));
-        assertEquals("book only time will tell  has been reserved",library.lendBook("only time will tell"));
+        assertEquals("book not found",library.lendBook("sins of the father"));
     }
+
 
 
 }
