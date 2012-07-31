21adf218c72e9b73f22176be01307a28106c121a
nittin4664 <nittin4664@gmail.com>
2012-07-31 12:06:25 +0530

    test for movieAsString passed

--- test/com/twu28/biblioteca/LibraryTest.java
+++ test/com/twu28/biblioteca/LibraryTest.java
@@ -17,5 +17,12 @@ public class LibraryTest extends TestCase {
         library.addMovieToShelf(new Movie("the dark night","christopher","9"));
         assertEquals("the dark night \t christopher \t 9|", library.moviesAsStings());
     }
+    @Test
+    public void testMoviesAsStingsForMoreThen1Movie(){
+        Library library=new Library();
+        library.addMovieToShelf(new Movie("the dark night","christopher","9"));
+        library.addMovieToShelf(new Movie("the dark night rises","christopher","8"));
+        assertEquals("the dark night \t christopher \t 9|the dark night rises \t christopher \t 8|", library.moviesAsStings());
+    }
 
 }
